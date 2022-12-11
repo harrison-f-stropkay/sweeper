ce5bb12b283e43d9bd34e2f4107857102019fea9
Harrison Stropkay <harrisonstropkay@icloud.com>
2022-12-11 16:05:36 -0500

    handled first guess bomb tile in reveal_tile

--- minesweeper-skeleton/minesweeper.py
+++ minesweeper-skeleton/minesweeper.py
@@ -5,15 +5,13 @@ import random
 
 
 class Minesweeper:
-    BOMB = -1
-    GAME_OVER = -2
-
     def __init__(self, tiles_height, tiles_width, num_bombs) -> None:
         self.tiles_height = tiles_height
         self.tiles_width = tiles_width
         self.num_bombs = num_bombs
         self.tiles = np.full([tiles_height, tiles_width], False)
         self.has_lost = False
+        self.first_guess = True
         
         # get (num_bombs + 1) random integers in our range using a partial Fisher–Yates shuffle
         sequence = np.arange(0, tiles_width * tiles_height)
@@ -29,63 +27,51 @@ class Minesweeper:
         # save the extra random tile in case the user selects a bomb tile on first guess
         self.extra_tile = divmod(sequence[num_bombs], tiles_width)
 
+    # returns value of tile or -1 if tile is a bomb
     def reveal_tile(self, y, x) -> int:
-        if self.has_lost:
-            return self.GAME_OVER
-        else:
+        # if first guess is a bomb tile, move that bomb to another tile
+        if self.first_guess:
+            self.first_guess = False
             if self.tiles[y, x]:
-                self.has_lost = True
-                return self.GAME_OVER
-            else:
-                count = 0
-                y_min, y_max = max(y - 1, 0), min(y + 1, self.tiles_height - 1)
-                x_min, x_max = max(x - 1, 0), min(x + 1, self.tiles_width - 1)
-                for y in range(y_min, y_max + 1):
-                    for x in range(x_min, x_max + 1):
-                        if self.tiles[y, x]:
-                            count += 1
-                return count
+                self.tiles[y, x] = False
+                self.tiles[self.extra_tile] = True
+
+        if self.has_lost:
+            return -1
+        elif self.tiles[y, x]:
+            self.has_lost = True
+            return -1
+        else:
+            count = 0
+            y_min, y_max = max(y - 1, 0), min(y + 1, self.tiles_height - 1)
+            x_min, x_max = max(x - 1, 0), min(x + 1, self.tiles_width - 1)
+            for y in range(y_min, y_max + 1):
+                for x in range(x_min, x_max + 1):
+                    if self.tiles[y, x]:
+                        count += 1
+            return count
 
     def __str__(self) -> str:
         # add top left corner space
-        result = " " * 2
+        result = buffer()
         # add x indices
         for x in range(self.tiles_width):
-            result += buffer('r', x)
+            result += buffer(x)
         result += "\n"
         # add y indices and tile values
         for y in range(self.tiles_height):
-            result += str(y) + " "
+            result += buffer(y)
             for x in range(self.tiles_width):
-                result += buffer('r', self.tiles[y, x])
+                if self.tiles[y, x]:
+                    result += buffer('*')
+                else:
+                    result += buffer('.')
             result += "\n"
         return result
 
 
-def buffer(direction, *args) -> str:
-    buffer_length = 7
+def buffer(*args) -> str:
     # set input to " " if input is not given as argument
     input = str(args[0]) if args else ""
-    match direction:
-        case 'l':
-            return input.ljust(buffer_length)
-
-        case 'r':
-            return input.rjust(buffer_length)
-
-        case 'c':
-            if(len(input) > buffer_length):
-                return input
-            else:
-                num_spaces = buffer_length - len(input)
-                num_spaces_left = num_spaces // 2
-                num_spaces_right = round(num_spaces / 2)
-                return ' ' * num_spaces_left + input + ' ' * num_spaces_right
-        # return error and usage if invalid direction is given
-        case _:
-            return "Error. Usage: buffer('l' | 'r', [input])"
-
-
-test_tiles = Minesweeper(10, 12, 15)
-print(test_tiles)
+    return input.ljust(3)
 
