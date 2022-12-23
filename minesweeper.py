a3a733a4b414c4404711e5c3a57a28ee5b300882
Harrison Stropkay <harrisonstropkay@icloud.com>
2022-12-23 02:15:09 -0500

    flipping and flagging guaranteed tiles working

--- minesweeper.py
+++ minesweeper.py
@@ -28,7 +28,8 @@ class Minesweeper:
 
 
 
-        self.borders = []
+        self.edges = []
+        self.waters = []
         self.known = np.full(self.game_tiles.shape, False)
 
     def swap_bomb(self, location) -> None:
@@ -40,7 +41,7 @@ class Minesweeper:
                     return
 
     def get_neighbor_locations(self, location) -> list[tuple]:
-        x, y = location[0], location[1]
+        x, y = location
         y_min, y_max = max(y - 1, 0), min(y + 1, self.height - 1)
         x_min, x_max = max(x - 1, 0), min(x + 1, self.width - 1)
         neighbor_locations = []
@@ -98,7 +99,6 @@ class Minesweeper:
         elif self.number_unflipped == 0:
             self.status = codes.WON
         # return
-        print("flipped tiles: ", flipped_tile_locations)
         return flipped_tile_locations
 
     def flag(self, location) -> None:
@@ -108,22 +108,25 @@ class Minesweeper:
         print(self)
         # game loop
         while self.status == codes.ONGOING:
-            cleaned_input = self.scan_guess()
-            # when input is of the form x y
-            if type(cleaned_input[0]) == int:
-                location = cleaned_input
-                if self.game_tiles[location] == codes.FLAG:
-                    self.game_tiles[location] = codes.UNFLIPPED
-                else:
-                    self.flip(cleaned_input)
-            # when input is of the form x y F
-            else:
-                location = cleaned_input[0]
+            location, command = self.prompt()
+            # input looks like: x y F
+            if command == "F":
                 if self.game_tiles[location] == codes.UNFLIPPED:
                     self.flag(location)
                 else:
                     print("Error. Cannot flag a revealed tile.")
+            # input looks like: G (or empty)
+            elif command == "G":
+                print(self.guess())
+            # input looks like: x y
+            else:
+                if self.game_tiles[location] == codes.FLAG:
+                    self.game_tiles[location] = codes.UNFLIPPED
+                else:
+                    self.flip(location)
+            # show player the current game state and update internals
             print(self)
+            self.update_edges(())                       ###
         # print concluding message
         if self.status == codes.WON:
             print("Game won!")
@@ -132,9 +135,11 @@ class Minesweeper:
             print("Game lost.")
             return False
     
-    def scan_guess(self) -> tuple:
+    def prompt(self) -> tuple:
         try:
             line = input("Guess: ")
+            if line in ["G", ""]:
+                return ((), "G")
             line_split = line.split()
             location = (int(line_split[0]), int(line_split[1]))
             if len(line_split) not in [2, 3]:
@@ -145,17 +150,17 @@ class Minesweeper:
                 raise Exception()
             if len(line_split) == 3:
                 if line_split[2] == "F":
-                    return (location, line_split[2])
+                    return (location, "F")
                 else:
                     raise Exception()
-            return location
+            return (location, "")
         except:
             print("Invalid input, try again. Usage: x y [F]. Example: '5 2' to flip tile at column 5, row 2")
-            return self.scan_guess()
+            return self.prompt()
             
     def __str__(self, mode="game"):
-        # add top left corner space
-        result = buffer()
+        # add top space and top left corner space
+        result = "\n" + buffer()
         # add x indices
         for x in range(self.width):
             result += buffer(x)
@@ -173,34 +178,86 @@ class Minesweeper:
 
 
 
-
-
+# TODO: compare to probability of random tile having a bomb, choose random if better
+# TODO: change everything to say location or opposite?
+# TODO: efficiency: return a list of the guarantees and act accordingly, not one at a time
+# TODO: improve update_edges
     
 
-    def update_borders(self, new_location) -> None:
-        self.known[new_location] = True
-        if not self.get_border(new_location):
-            pass
 
-    # returns the confidence in the guess 
+    # returns the confidence in the guess and the location
     def guess(self) -> float:
-        for border in self.borders:
-            for location in border:
-                guarantee_status = self.is_guaranteed(location)
-                if guarantee_status[0]:
-                    return 1
+        # see if there is a guaranteed bomb or safe tile
+        for edge in self.edges:
+            for location in edge:
+                if (potential_neighbor := self.get_guarantee_neighbor(location)) == None:
+                    continue
+                assessment, location = potential_neighbor
+                if assessment == codes.BOMB:
+                    self.flag(location)
+                    return float(1)
+                if assessment == codes.NOT_BOMB:
+                    self.flip(location)
+                    return float(1)
+        
+        # otherwise, flip the most likely safe tile
         return 0
 
 
-    def is_guaranteed(self, location) -> tuple:
-        return ()
-            
-
-    def get_border(self, location) -> set | None:
-        for border in self.borders:
-            if location in border:
-                return border
+    def get_guarantee_neighbor(self, location) -> tuple | None:
+        tile_value = self.game_tiles[location]
+        number_flags = 0
+        number_unflipped = 0
+        # get info about neighbors
+        for neighbor in self.get_neighbor_locations(location):
+            if self.game_tiles[neighbor] == codes.FLAG:
+                number_flags += 1
+            elif self.game_tiles[neighbor] == codes.UNFLIPPED:
+                number_unflipped += 1
+        # if the info guarantees a tile value, return such an assessment  
+        if tile_value in (number_flags, number_flags + number_unflipped):
+            for neighbor in self.get_neighbor_locations(location):
+                if self.game_tiles[neighbor] == codes.UNFLIPPED:
+                    if tile_value == number_flags:
+                        return (codes.NOT_BOMB, neighbor)
+                    else:
+                        return (codes.BOMB, neighbor)
+        # when there are no obvious neighbor values
         return None
+
+
+    def update_edges(self, location) -> None:
+        one_big_edge = set()
+        for x in range(self.width):
+            for y in range(self.height):
+                if self.is_edge_tile((x, y)):
+                    one_big_edge.add((x, y))
+        self.edges = [one_big_edge]
+
+
+    def is_edge_tile(self, location) -> bool:
+        has_flipped_neighbor = False
+        has_unflipped_neighbor = False
+        for neighbor in self.get_neighbor_locations(location):
+            if self.game_tiles[neighbor] not in (codes.UNFLIPPED, codes.FLAG):
+                has_flipped_neighbor = True
+            elif self.game_tiles[neighbor] == codes.UNFLIPPED:
+                has_unflipped_neighbor = True
+        return has_flipped_neighbor and has_unflipped_neighbor
+
+        
+
+        
+            
+    
+
+
+
+    # def get_edge(self, location) -> set | None:
+    #     for border in self.edges:
+    #         if location in border:
+    #             return border
+    #     return None
         
     
 
@@ -210,5 +267,7 @@ def buffer(*args) -> str:
 
 
 
-test = Minesweeper(10, 10, 12)
+test = Minesweeper(30, 16, 99)
+# expert:
+# test = Minesweeper(30, 16, 99)
 test.play()
